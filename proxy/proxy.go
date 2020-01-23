54fcfb03873d7fd306b2fd9ecb8fe4390ac2e431
Johannes Senner <joda@bestbytes.de>
2020-01-23 12:26:27 +0100

    fix: handle legacy routes that used to include workspace query parameter

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -94,6 +94,20 @@ func (p *Proxy) middlewareTokenAuth(next http.Handler) http.Handler {
 	})
 }
 
+func (p *Proxy) middlewareWorkspace(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		workspace := r.URL.Query().Get("workspace")
+		if workspace != p.config.Neos.Workspace && workspace != "" {
+			p.error(w, r, http.StatusBadRequest, "the selected workspace does not match the neosproxy configuration")
+			return
+		}
+
+		// call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
 func (p *Proxy) middlewareBasicAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
--- proxy/routes.go
+++ proxy/routes.go
@@ -19,24 +19,34 @@ func (p *Proxy) setupRoutes() {
 
 	// content tree / sitemap
 	p.router.HandleFunc(routeContentServerExport, p.streamCachedSitemap)
+	p.router.HandleFunc(routeContentServerExport, p.streamCachedSitemap).Queries("workspace", "{workspace}")
 
 	// etag
 	p.router.HandleFunc(routeContentServerExport+"/etag/{dimension}/{id}", p.getEtagByID).Methods(http.MethodGet)
+	p.router.HandleFunc(routeContentServerExport+"/etag/{dimension}/{id}", p.getEtagByID).Methods(http.MethodGet).Queries("workspace", "{workspace}")
 	p.router.HandleFunc(routeContentServerExport+"/etag/{hash}", p.getEtagByHash).Methods(http.MethodGet)
 
 	p.router.HandleFunc(routeContentServerExport+"/etags", p.getAllEtags).Methods(http.MethodGet)
+	p.router.HandleFunc(routeContentServerExport+"/etags", p.getAllEtags).Methods(http.MethodGet).Queries("workspace", "{workspace}")
 
 	// documents => /contentserver/export/de/571fd1ae-c8e4-4d91-a708-d97025fb015c?workspace=stage
 	p.router.HandleFunc(routeContentServerExport+"/{dimension}/{id}", p.getContent).Methods(http.MethodGet)
+	p.router.HandleFunc(routeContentServerExport+"/{dimension}/{id}", p.getContent).Methods(http.MethodGet).Queries("workspace", "{workspace}")
 
 	p.router.HandleFunc(routeContentServerExport+"/{dimension}/{id}", p.getEtagByID).Methods(http.MethodHead)
+	p.router.HandleFunc(routeContentServerExport+"/{dimension}/{id}", p.getEtagByID).Methods(http.MethodHead).Queries("workspace", "{workspace}")
+
+	// workspace middleware
+	p.router.Use(p.middlewareWorkspace)
 
 	// api
 	// neosproxy/cache/%s?workspace=%s
 	neosproxyRouter := p.router.PathPrefix(neosproxyPath).Subrouter()
 	neosproxyRouter.Use(p.middlewareTokenAuth)
+	neosproxyRouter.Use(p.middlewareWorkspace)
 	neosproxyRouter.HandleFunc("/cache/all", p.invalidateCacheAll).Methods(http.MethodDelete)
 	neosproxyRouter.HandleFunc("/cache/{id}", p.invalidateCache).Methods(http.MethodDelete)
+	neosproxyRouter.HandleFunc("/cache/{id}", p.invalidateCache).Methods(http.MethodDelete).Queries("workspace", "{workspace}").Name("api-delete-cache")
 	neosproxyRouter.HandleFunc("/status", p.streamStatus).Methods(http.MethodGet)
 
 	// error handling
